f149d2c54bb0ccc0c481e9459c80696a0f19e791
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2023-02-11 22:50:02 +0500

    title section is created

---
+++ app.js
@@ -0,0 +1,6 @@
+// $(".search-input").hide();
+$(".search-btn").mouseenter(function(){
+    $(".search-btn").empty().append('<input class="form-control me-2 search-input" type="search" placeholder="Search">');});
+
+    
+ 
