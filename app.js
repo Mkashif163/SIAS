f9ac6518580650feb92190670e73dc902b5f3bff
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2023-02-12 20:52:40 +0500

    login page created

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-// $(".search-input").hide();
-$(".search-btn").mouseenter(function(){
-    $(".search-btn").empty().append('<input class="form-control me-2 search-input" type="search" placeholder="Search">');});
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
 
     
  
--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-// $(".search-input").hide();
+
 $(".search-btn").mouseenter(function(){
     $(".search-btn").empty().append('<input class="form-control me-2 search-input" type="search" placeholder="Search">');});// $(".search-input").hide();
     $(".search-btn").mouseenter(function(){
     $(".search-btn").empty().append('<input class="form-control me-2 search-input" type="search" placeholder="Search">');});
+
+$(".signme").click(function () { 
+    $("title").text("SIAS Signup");
+});
+$(".logme").click(function () { 
+    $("title").text("SIAS Login");
+});
+$(document).ready(function() {
+    $('#teacherCheckbox').click(function() {
+      $('#studentCheckbox').prop('checked', false);
+    });
+    $('#studentCheckbox').click(function() {
+      $('#teacherCheckbox').prop('checked', false);
+    });
+  });
