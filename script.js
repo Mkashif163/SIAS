16a7032e706a9d411ca0118155a9daea7ed2751c
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2023-02-12 19:17:09 +0500

    making log in page

---
+++ script.js
@@ -0,0 +1,5 @@
+// $(".search-input").hide();
+$(".search-btn").mouseenter(function(){
+    $(".search-btn").empty().append('<input class="form-control me-2 search-input" type="search" placeholder="Search">');});// $(".search-input").hide();
+    $(".search-btn").mouseenter(function(){
+    $(".search-btn").empty().append('<input class="form-control me-2 search-input" type="search" placeholder="Search">');});
