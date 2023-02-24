082f95e30231ad7b811c4f399e38012d2f14ccff
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2023-02-24 19:53:28 +0500

    teacher page added

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ $(".signme").click(function () {
 });
 $(".logme").click(function () { 
     $("title").text("SIAS Login");
-});
+});  
+
+// log in check boxex teacher of student to check one at a time
 $(document).ready(function() {
     $('#teacherCheckbox').click(function() {
       $('#studentCheckbox').prop('checked', false);
@@ -18,3 +20,23 @@ $(document).ready(function() {
       $('#teacherCheckbox').prop('checked', false);
     });
   });
+
+// pop for create claass
+$(document).ready(function() {
+  // When the "Create Class" button is clicked, get the value of the class name input and create a new card
+  $('#createClassBtn').click(function() {
+    var className = $('#classNameInput').val();
+    var newCard = '<div class="card m-3" style="width: 18rem; min-width: 16rem;">' +
+                  '<i class="card-img-top fa-solid fa-folder-open fa-8x p-lg-5"></i>' +
+                  '<div class="card-body">' +
+                  '<p class="card-text">' + className + '</p>' +
+                  '</div>' +
+                  '</div>';
+    $('.card-append').append(newCard);
+    // Close the modal after the new card is created
+    $('#exampleModal').modal('hide');
+  });
+});
+
+
+
